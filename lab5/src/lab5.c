5b2d41f610eec226aa8722e49fb4b36de6f395e0
Churbanov <31795167+Churbanov@users.noreply.github.com>
2017-09-09 15:19:57 +0300

    init
    All 3 labs, docs

---
+++ lab5/src/lab5.c
@@ -0,0 +1,24 @@
+﻿#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+int main(int argc, const char * argv[], const char *envp[])
+{
+    char data[8000];
+    strcat(data, "CLI arguments\n" );
+    for (int i=0; i<argc; i++) {
+        strcat(data, argv[i]);
+        strcat(data, "\n");
+    }
+    strcat(data, "Enviroment variables\n" );
+    for (int i=0; envp[i]; i++) {
+        strcat(data, envp[i]);
+        strcat(data, "\n");
+        
+    }
+    printf("%s",data);
+    
+}
